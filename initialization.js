701ca634962cadd4bb165ce0a6df7795749690be
hamishtodd1 <s1560142@SCE-BIO-C04326.bio.ed.ac.uk>
2015-11-03 16:40:23 +0000

    still not ok

--- initialization.js
+++ initialization.js
@@ -141,7 +141,7 @@ function init() {
 	
 	//-------------stuff that goes in the scene
 	{
-		var backgroundtexture_file = THREE.ImageUtils.loadTexture( 'adenovirus256.jpg', {}, function() { renderer.render(scene);});
+		var backgroundtexture_file = THREE.ImageUtils.loadTexture( 'adenovirus256.jpg', {}, function() { renderer.render(scene, camera);});
 		var backgroundtexture_material = new THREE.MeshBasicMaterial({
 			map: backgroundtexture_file,
 //			color:0x000000,
@@ -160,7 +160,7 @@ function init() {
 		backgroundtexture_geometry.setIndex(new THREE.BufferAttribute( backgroundtexture_triangle_vertices, 1 ) );
 		backgroundtexture_geometry.addAttribute( 'position', new THREE.BufferAttribute( backgroundtexture_vertices_numbers, 3 ) );
 		backgroundtexture = new THREE.Mesh( backgroundtexture_geometry, backgroundtexture_material );
-		console.log("tweasfuasasdffd")
+		console.log("astweasfuasasdffd")
 		
 		var surfacematerial = new THREE.MeshBasicMaterial({
 			color: 0x00ffff,
