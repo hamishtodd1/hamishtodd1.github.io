b69a29b8dd890d5da527d08781ab7dbc2261f118
hamishtodd1 <s1560142@SCE-BIO-C04326.bio.ed.ac.uk>
2015-11-03 16:26:22 +0000

    fasd

--- initialization.js
+++ initialization.js
@@ -141,7 +141,7 @@ function init() {
 	
 	//-------------stuff that goes in the scene
 	{
-		var backgroundtexture_file = THREE.ImageUtils.TextureLoader( 'adenovirus256.jpg', undefined, function ( tex1 ) {
+		var backgroundtexture_file = THREE.ImageUtils.loadTexture( 'adenovirus256.jpg', undefined, function ( tex1 ) {
 		    var i2 = THREE.ImageUtils.loadTexture('adenovirus256.jpg', undefined, function ( tex2 ) {});
 		});
 		console.log(backgroundtexture_file );
@@ -164,7 +164,7 @@ function init() {
 		backgroundtexture_geometry.setIndex(new THREE.BufferAttribute( backgroundtexture_triangle_vertices, 1 ) );
 		backgroundtexture_geometry.addAttribute( 'position', new THREE.BufferAttribute( backgroundtexture_vertices_numbers, 3 ) );
 		backgroundtexture = new THREE.Mesh( backgroundtexture_geometry, backgroundtexture_material );
-		console.log("asdsfd")
+		console.log("asfuasasdffd")
 		
 		var surfacematerial = new THREE.MeshBasicMaterial({
 			color: 0x00ffff,
