0acedcf300ef8fa0f5ec4621657cc65ef6254c55
hamishtodd1 <s1560142@SCE-BIO-C04326.bio.ed.ac.uk>
2015-11-11 18:24:12 +0000

    guh

--- initialization.js
+++ initialization.js
@@ -146,11 +146,11 @@ function init() {
 			texture_loader.load(
 				picture_properties[i].name, //they need to be powers of 2!
 				function(texture,i) {
-					picture_objects[lowest_unused_slideindex ] = new THREE.Mesh( new THREE.CubeGeometry( playing_field_width / 2, playing_field_width / 4, 0), 
-														 new THREE.MeshBasicMaterial({map: texture}) );
-					picture_objects[lowest_unused_slideindex ].position.x = picture_properties[lowest_unused_slideindex].x - 2; //increase or decrease this to center pic 1
-					picture_objects[lowest_unused_slideindex ].position.y = picture_properties[lowest_unused_slideindex].y;
-					lowest_unused_slideindex++;
+					picture_objects[lowest_unused_picindex ] = new THREE.Mesh( new THREE.CubeGeometry( playing_field_width / 2, playing_field_width / 4, 0), 
+							 new THREE.MeshBasicMaterial({map: texture}) );
+					picture_objects[lowest_unused_picindex ].position.x = picture_properties[lowest_unused_picindex].x - 2; //increase or decrease this to center pic 1
+					picture_objects[lowest_unused_picindex ].position.y = picture_properties[lowest_unused_picindex].y;
+					lowest_unused_picindex++;
 				},
 				function ( xhr ) {}, function ( xhr ) {console.log( 'texture loading error' );}
 			);
