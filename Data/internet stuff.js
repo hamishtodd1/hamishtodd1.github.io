6d067deb621f6d2e0762b9199afc7c05531d7bc9
Hamish Todd <Hamish Todd>
2016-04-07 21:01:32 +0100

    Internet stuff overhauled, pictures separated out

--- Data/internet stuff.js
+++ Data/internet stuff.js
@@ -1,66 +1,12 @@
+//----------youtube stuff
 var tag = document.createElement('script');
 tag.src = "https://www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-var pictures_imported = Array(0,0,0,0, 0,0,0,0);
-
 var section_finishing_time = Array(29,177,295,548,738,909,animation_beginning_second,99999999999);
 var pausing_times = Array(36.3,185,321.3,550,747.9,931);
 var secondsthroughvid = 0;
-
-var texture_loader = new THREE.TextureLoader();
-
-var picture_properties = Array(8);
-
-picture_properties[0] = {}; picture_properties[0].name = "Data/1 - BV.jpg";
-picture_properties[1] = {}; picture_properties[1].name = "Data/3 - PV.png";
-picture_properties[2] = {}; picture_properties[2].name = "Data/4 - SFV.png";
-picture_properties[3] = {};	picture_properties[3].name = "Data/7 - CV.png";
-picture_properties[4] = {}; picture_properties[4].name = "Data/9 - N4.gif";
-picture_properties[5] = {};	picture_properties[5].name = "Data/12 - RVFV.png";
-picture_properties[6] = {}; picture_properties[6].name = "Data/13 - RV.png";
-picture_properties[7] = {}; picture_properties[7].name = "Data/warning.png";
-
-var picture_loaded = 0;
-
-function loadpic(i) {
-	console.log(i);
-	texture_loader.load(
-		picture_properties[i].name,
-		function(texture) {
-			var mywidth = 3;
-			if(picture_loaded<7){
-				var myscale = 2;
-				picture_objects[picture_loaded] = new THREE.Mesh(new THREE.CubeGeometry(mywidth , mywidth , 0),new THREE.MeshBasicMaterial({map: texture, transparent:true}) );
-
-				picture_objects[picture_loaded].position.x = -playing_field_width / 2 + 1.2;
-				picture_objects[picture_loaded].position.y = playing_field_width / 2 - 1.2;
-				picture_objects[picture_loaded].position.z = 0.01;
-			}
-			else{
-				picture_objects[picture_loaded] = new THREE.Mesh(new THREE.CubeGeometry(playing_field_width, playing_field_width, 0),new THREE.MeshBasicMaterial({map: texture}) );
-			}
-			//so they're offscreen by default
-			
-			console.log(picture_loaded);
-			picture_loaded++;
-
-			if(picture_loaded < picture_properties.length )
-				loadpic(picture_loaded);
-			else {
-				console.log("done");
-
-				//ready to rock!
-				MODE = 0;
-				init(); //TODO call this before you start loading the pictures
-				scene.add(picture_objects[7]);
-				render();
-			}
-		},
-		function ( xhr ) {}, function ( xhr ) {console.log( 'texture loading error' );}
-	);
-}
 
 function react_to_video(){
 	/* Note that it is *finishing* time
@@ -102,7 +48,8 @@ function react_to_video(){
 
 function onPlayerReady(event) {
 	//start importing all pictures
-	loadpic(0);
+	loadpics();
+	init();
 }
 
 /* _MF2DVU8oB0: tall video
@@ -129,4 +76,14 @@ function onYouTubeIframeAPIReady(){
 			modestbranding: 1,
 		}
 	});
+}
+
+function attempt_launch(){
+	if( !INITIALIZED || !PICTURES_LOADED )
+		return;
+	
+	MODE = 0;
+	ChangeScene(MODE);
+	scene.add(picture_objects[0]);
+	render();
 }
