4e2324277a7a8bbf952421ca74723ae2a177f9ee
Hamish Todd <Hamish Todd>
2016-10-31 17:16:50 +0100

    back to previous protein

--- Cardboard/NGLgetter.js
+++ Cardboard/NGLgetter.js
@@ -2,7 +2,7 @@ var loose_surface;
 function get_NGL_protein()
 {
 	//Trp-Cage Miniprotein Construct TC5b, 20 residues: 1l2y. Rubisco: 1rcx. Insulin: 4ins
-	var testproteinlink = "http://files.rcsb.org/download/1QGT.pdb";
+	var testproteinlink = "http://files.rcsb.org/download/4ins.pdb";
 	var testobjlink = "http://threejs.org/examples/obj/male02/male02.obj";
 		
 	var xhr = new XMLHttpRequest();
