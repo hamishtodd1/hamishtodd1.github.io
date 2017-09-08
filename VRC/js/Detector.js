30acc2f0ccc7aecfcf468fea39e0836266d16e05
Hamish <hamish.todd1@gmail.com>
2017-09-08 15:43:00 +0100

    all working, turns out this was a bad idea4

--- VRC/js/Detector.js
+++ VRC/js/Detector.js
@@ -3,7 +3,7 @@
  * @author mr.doob / http://mrdoob.com/
  */
 
-var Detector = {
+var Detector = { 
 
 	canvas: !! window.CanvasRenderingContext2D,
 	webgl: ( function () { try { var canvas = document.createElement( 'canvas' ); return !! window.WebGLRenderingContext && ( canvas.getContext( 'webgl' ) || canvas.getContext( 'experimental-webgl' ) ); } catch( e ) { return false; } } )(),
