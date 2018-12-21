ab219ed744df8ce4071450473d778c03c5bb0ac2
hamishtodd1 <hamish.todd1@gmail.com>
2018-12-21 17:49:17 +0000

    extra idea

---
+++ zeimLight/units/cogs.js
@@ -0,0 +1,59 @@
+/*
+background shouldn't be boring black, make it like that demoscene dude's lectures
+
+cogs could have some visual character? rust, chipped teeth, patterend holes, handle on one, windmill on another?
+
+make animated gifs of all of them
+
+Simply do it as a tumblr post.
+
+Would be kinda awesome if you could morph things. truncated cuboctahedron to truncated icosidodecahedron
+
+Script:
+
+Question: how can we arrange equally-sized cogs so that they are able to turn?
+
+[have some pictures]
+
+General principle: Cogs that touch one another must go in opposite directions.
+
+[have a line of five cogs, red yellow red yellow]
+
+Suppose we wish to have arrangements of cogs that close up.
+
+[the five cogs try to turn into a pentagon but there is chafing. We add another cog, allowing the creation of a hexagon]
+
+Therefore we require cogs to be arranged around polygons with an even number of sides.
+
+[square, octagon, dodecagon. Maybe concentric]
+
+We ask: "what can be made with even-sided polygons?"
+
+Answer: some cool patterns, and some 3D shapess that deserve a bit of love!
+
+Unless we want different-size cogs, we are talking about archimedean solids
+
+[cube. From now, writing appears below pics] Easy start: here the cogs are place on the corner of a cube
+
+[truncated octahedron] the shape that is implied by these cogs is a "truncated octahedron". [writing disappears, is replaced by the following] Note that the cogs are arranged around hexagons and squares.
+
+[truncated cuboctahedron] This one is made of squares, hexagons and octagons. It's called a "truncated cuboctahedron", but if you look it up you'll see that that's a rather misleading name for it.
+
+[truncated icoisdodecahedron] Now we're onto squares, hexagons and decagons. The natural thing to do now is to go onto squares, hexagons and dodecagons...
+
+[Great rhombitrihexagonal tiling] But when we bring those shapes together, we get a flat pattern rather than a solid. This is the "Great rhombitrihexagonal tiling"...
+
+[Truncated square tiling] ...and this is really the only other interesting tiling.
+
+[decagonal prism] There are some other solids we can make. This is a boring example, a "decagonal prism". [replace with hexadecagonal prism] You can also have arbitrary prisms (duh)
+
+[rhombic tricontahedron] Here the cogs are on the corners of diamonds, so we can make this "rhombic tricontahedron". Sometimes 3 diamonds meet at a corner, sometimes five.
+
+[rhombic dodecahedron] And here we have a rhombic dodecdahedron, which has a similar principle. Fun fact: rhombic dodecahedrons can tesselate 3-space!
+
+[truncated rhombic dodecahedron] This is a rhombic dodecahedron with six corners cut off and replaced with squares, a "truncated rhombic dodecahedron".
+
+With rhombuses, in theory you should be allowed to have six meet 
+
+(here we have limited ourselves to shapes where a cog touches at least three other cogs)
+*/
---
+++ zeimLight/units/pwg.js
@@ -0,0 +1,245 @@
+/*
+	The main point of this is to make something that visualizes how your shit works, show it to the audience, and it's not code, all geometrical
+		The way you show things should be similar to the way you program them; otherwise illustrating how they were made is extra work
+	Another point is to let you make geometries with specific behaviour
+		You want to be able to do things gesturally, at runtime, changing values
+		Could just use a 3D software package
+
+	Possibly there could be some type inference, you do something vague like conenct one object to another and it works out based on types what you want. Kiiiinda like at text editor
+
+	Would *like* the bret thing of changing initial conditions and having it simulate forward with the same input you just gave to get to current position
+
+	The only "real" objects in the scene are meshes, which are a geometry and a material
+		How do they actually interact?
+		A line connecting a property to its object (if it has a visualization)
+			The dots eg object.property in javascript
+			No, nothing extra to add here, because that is the point - if you want to see what it affects, see it do things
+			Or maybe this is a good idea? Don't want the object itself minimized to be in those balls
+
+	A position as a set of three arrow lengths, like in eugene kutoryansky
+		It's always there, sitting on the tip of the last arrow
+	
+	Forget about the contents of object.geometry and material properties beyond color
+		What that leaves for 3D objects is the matrix and RGB
+		The object is not what you normally think, this is not the game itself.
+			Its position is separate from it, and you update that. Then that updates it.
+		the object is a set of triangles. It just gets the balls shot at at.
+			And we don't imagine its matrix, that's for "the programmer", like assembly language
+
+	"position" shooting its ball at it could do the equivalent of applyMatrix(makeTranslation)
+
+	TODO
+		Focus on positions, and making animations
+		Want to snap to the nearest receptacle
+			Receptacles should be made by clicking empty space
+		The location of the things should be decided automatically =/ moving shit around in blueprints was bad, like indenting
+*/
+
+function initPwg()
+{
+	// primitiveValueRepresentations.number = new THREE.Mesh(new THREE.PlaneGeometry(1,1),new THREE.MeshBasicMaterial({color:0xFF0000}))
+	//tanh(actual value) is what you want
+	//or maybe the symbols are wanted
+
+	let editingMode = true
+	dataAbstractions = []
+	bindButton("space",function()
+	{
+		editingMode = !editingMode
+		for(let i = 0; i < dataAbstractions.length; i++)
+		{
+			dataAbstractions[i].visible = editingMode
+		}
+	},"editor mode")
+
+	let radius = 0.1;
+	let stickOnRadius = radius * 0.9;
+	let barrelRadius = radius / 3;
+	{
+		let barrelThickness = barrelRadius / 10;
+		let barrelExtendTo = radius * 2;
+		let barrelPoints = [
+			new THREE.Vector2(barrelRadius-barrelThickness,stickOnRadius
+				),
+			new THREE.Vector2(barrelRadius,stickOnRadius
+				),
+			new THREE.Vector2(barrelRadius,barrelExtendTo),
+			new THREE.Vector2(barrelRadius-barrelThickness,barrelExtendTo),
+			new THREE.Vector2(barrelRadius-barrelThickness,stickOnRadius
+				),
+		];
+		var barrelGeometry = new THREE.LatheGeometry( barrelPoints, 31 )
+		barrelGeometry.applyMatrix(new THREE.Matrix4().makeRotationX(TAU/4))
+	}
+
+	let booleanGeometry = new THREE.BoxGeometry(radius/2,radius/2,radius/2)
+
+	{
+		let trumpetRadius = radius / 3;
+		let trumpetThickness = trumpetRadius / 10;
+		let trumpetPoints = [];
+		let neckRadius = barrelRadius;
+		let fanningCenter = new THREE.Vector2(radius*2,stickOnRadius)
+		let fanFullAngle = 0.65;
+		let numPoints = 14;
+		for(let i = 0; i < numPoints; i++)
+		{
+			let newPoint = new THREE.Vector2(neckRadius,stickOnRadius,0);
+			newPoint.rotateAround(fanningCenter, -i * fanFullAngle / numPoints);
+			trumpetPoints.push(newPoint);
+		}
+		var trumpetGeometry  = new THREE.LatheGeometry( trumpetPoints, 31 )
+		trumpetGeometry.applyMatrix(new THREE.Matrix4().makeRotationX(TAU/4));
+	}
+
+	function DataAbstraction(value)
+	{
+		let dataAbstraction = new THREE.Mesh(new THREE.EfficientSphereGeometry(radius), new THREE.MeshPhongMaterial( { transparent:true, opacity: 0.6 } ) );
+
+		let type = typeof value;
+		dataAbstraction.type = type
+		if(type === "object")
+		{
+			if( THREE.Vector3.prototype.isPrototypeOf(value) )
+			{
+				//put it in there, right? But shouldn't it stay in place? =/ maybe have a little represenation of the scene?
+			}
+			else if( THREE.Object3D.prototype.isPrototypeOf(value) )
+			{
+				//again little version of the scene?
+			}
+			else if( THREE.Quaternion.prototype.isPrototypeOf(value) )
+			{
+				//er, the object that it is a quaternion of in that orientation?
+				//if it has no assigned object use the stanford teapot
+				//Only works in VR
+			}
+			else
+			{
+				//text I guess?
+			}
+		}
+
+		console.assert(type === "boolean")
+		{
+			dataAbstraction.valueRepresentation = new THREE.Mesh( booleanGeometry )
+			dataAbstraction.add(dataAbstraction.valueRepresentation)
+
+			dataAbstraction.setValue = function(newValue)
+			{
+				dataAbstraction.valueRepresentation.material.color.setHex( newValue ? 0xFFFFFF : 0x000000 )
+			}
+			dataAbstraction.setValue(value)
+
+			dataAbstraction.getValue = function()
+			{
+				return dataAbstraction.valueRepresentation.material.color.r === 1.0 && dataAbstraction.valueRepresentation.material.color.g === 1.0 && dataAbstraction.valueRepresentation.material.color.b === 1.0;
+			}
+		}
+
+		dataAbstraction.target = null;
+		dataAbstraction.setTarget = function(newTarget)
+		{
+			if(newTarget.type !== type)
+			{
+				console.error("Type error")
+				return
+			}
+			target = newTarget
+		}
+
+		let barrel = new THREE.Mesh( barrelGeometry, new THREE.MeshPhongMaterial( { color: 0x808080 } ) );
+		dataAbstraction.add( barrel );
+
+		let trumpet = new THREE.Mesh( trumpetGeometry, 
+			new THREE.MeshPhongMaterial( { 
+				color: 0x808080, 
+				/*transparent:true, opacity:0.6 */ 
+			} ) );
+		trumpet.rotation.x = -TAU/4;
+		dataAbstraction.add(trumpet)
+
+		updateFunctionsToBeCalled.push(function()
+		{
+			if(dataAbstraction.visible)
+			{
+				if(dataAbstraction.target !== null)
+				{
+					let targetPositionLocal = dataAbstraction.target.position.clone();
+					dataAbstraction.worldToLocal(targetPositionLocal);
+					barrel.lookAt(targetPositionLocal)
+				}
+
+				let cameraPositionLocal = camera.position.clone();
+				dataAbstraction.worldToLocal(cameraPositionLocal);
+				dataAbstraction.valueRepresentation.lookAt(cameraPositionLocal)
+			}
+
+			//definitely happenning in wrong order
+			if( dataAbstraction.target !== null )
+			{
+				dataAbstraction.target.setValue(value)
+			}
+		})
+
+		return dataAbstraction;
+	}
+
+	let dataAbstractionA = DataAbstraction(true);
+	dataAbstractionA.position.set(-0.5,0,-0.5)
+	scene.add(dataAbstractionA)
+
+	let dataAbstractionB = DataAbstraction(false);
+	dataAbstractionB.position.set(0.5,0,-0.5)
+	scene.add(dataAbstractionB)
+
+	dataAbstractionA.target = dataAbstractionB;
+
+	dataAbstractions.push(dataAbstractionA,dataAbstractionB)
+
+	let actualObject = new THREE.Mesh(new THREE.BoxGeometry(0.1,0.2,0.3), new THREE.MeshPhongMaterial({color:0xFF0000}))
+	actualObject.position.y = -0.3
+	scene.add(actualObject)
+
+	return;
+
+	//two trumpets one gun
+	let multiplier = new THREE.Object3D();
+	let adder = new THREE.Object3D();
+	let pow = new THREE.Object3D();
+
+	//one, one, can make the first three yourself
+	let sqrt = new THREE.Object3D();
+	let sin = new THREE.Object3D();
+	let cos = new THREE.Object3D();
+	let tan = new THREE.Object3D();
+	let asin = new THREE.Object3D();
+	let acos = new THREE.Object3D();
+	let atan = new THREE.Object3D();
+
+	//one trumpet on top of the other because that's the convention (there's no logic to it probably)
+	let divider = new THREE.Object3D(); 
+	let subtractor = new THREE.Object3D();
+
+	//0 trumpets, 1 gun
+	let random = new THREE.Object3D();
+	let frameStarter = new THREE.Object3D(); //aimed at the first object to be updated
+
+	//Two trumpets but what comes out is a boolean. Construct others out of these. There's probably a compiler optimization for <= but it serves as a good example
+	let conditional = new THREE.Object3D(); 
+	let and = new THREE.Object3D(); 
+	let or = new THREE.Object3D();
+	let not = new THREE.Object3D();
+	let equalTo = new THREE.Object3D(); 
+	let greaterThan = new THREE.Object3D();
+	let lessThan = new THREE.Object3D();
+
+	//one trumpet in, an array. Three guns: loop body, index, onComplete
+	let forLoop = new THREE.Object3D();
+
+	//array goes in, number comes out 
+	let min = new THREE.Object3D();
+	let max = new THREE.Object3D();
+
+	//to bootstrap: increment, decrement, absolute value, greaterThanOrEqualTo, lessThanOrEqualTo
+}
