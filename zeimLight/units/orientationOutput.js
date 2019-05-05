df5881fc7df6b359bdf397081529e96cb1f846b4
hamishtodd1 <hamish.todd1@gmail.com>
2019-05-05 22:15:05 +0100

    woo done maybe

--- zeimLight/units/orientationOutput.js
+++ zeimLight/units/orientationOutput.js
@@ -1,5 +1,29 @@
 function initOrientationOutput()
 {
-	let sign = makeTextSign("hello")
-	scene.add(sign)
+	let object = new THREE.Object3D()
+	object.quaternion.set( Math.random(),Math.random(),Math.random(),Math.random() )
+	let controls = new THREE.DeviceOrientationControls( object );
+
+	for(let i = 0; i < 4; i++)
+	{
+		f(i)
+	}
+	log(object.quaternion.toArray())
+	
+
+	function f(i)
+	{
+		let str = (object.quaternion.toArray())[i].toString()
+
+		let sign = makeTextSign(str)
+		sign.scale.multiplyScalar(0.17)
+		sign.position.y = 0.3 - i * 0.2
+		scene.add(sign)
+
+		updateFunctions.push(function()
+		{
+			str = (object.quaternion.toArray())[i].toString()
+			sign.children[0].material.setText(str)
+		})
+	}
 }
