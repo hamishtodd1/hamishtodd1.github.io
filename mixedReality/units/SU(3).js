7c84dfc02f4c63b8b26f0b739f2794044298fe4c
hamish <hamish.todd1@gmail.com>
2019-05-28 10:41:27 +0100

    adding su3

---
+++ mixedReality/units/SU(3).js
@@ -0,0 +1,66 @@
+/*
+	Group	Acts on		Manifold st point = element		Can be broken up into sets looking like...	Such that all the elements in a set preserve	...and therefore the base space is
+	SU(2)	R3 / C2		S3								S1s											The "axis", a 1D subspace						S2
+	SE(3)	R6 / C3		Something 8D, unusable			S3s											Another 1D subspace?							S5
+
+	If a fish wanted to think a little about the structure of SU(2);
+	equipped with a visualization of three colored points moving on lines, depicting the xyz of some points on a sphere,
+	The fish could choose one of the sets of points
+	And do some rotating. The xyz of the grabbed point wouldn't change; they'd be exploring one of the S1s in the hopf fibration
+
+	The ordinary way that the fish preserves a subspace, with the projection, is to go over to that projected point and grab and rotate around it
+		Which corresponds to choosing a subspace
+
+
+	So fish wants to understand hopf fibration (not necessarily its 3D stereographic projection!)
+		It explores S2, eg chooses a hopf circle by moving around a stereographic projection of S2
+		Fibers do group together into clifford toruses. Could alternate between circles and that.
+		The connection to SO(4) is coincidental, there is no equivalent for SU(3), so forget about that
+			But the idea of choosing a stabilizer, eg a point in the base space, makes sense
+			It's a metaphor, but not an exact mapping
+			The hopf fibration is a visualization of the structure of rotations that is appreciable to a 3D creature, that is nice
+			But don't think that it's real. T
+		It grabs a point to preserve (=a 1D subspace of R3) and then explores that circle by rotating
+		Is there some nice way to explore S5? you have 6dof. And then it's quite easy to move around S3, a mere 3-dof
+		It might even have points projected from S2, and little circles
+			But you have to choose how many circles, eg probably choose some discrete subgroup
+		an advantage it has is that the base space has as many dimensions as its universe
+			2d creature tries to understand s2 vs 3D understand S5
+			Where are the numbers for these base spaces coming from?
+				2 = 
+				5 = 3*2 - 1
+			sooo, not allowed stereographic projection
+		So no, the fish is not going around a stereographic projection this time
+		It has a square slice and a slider... jeez though, it's only looking for a 1D subspace, so wasteful
+
+		Maybe need both hands somehow
+
+		Linelander learns about 3D orientations?
+			It has 2 views of the sphere in patches that are at right angles
+
+			It can go forwards and it can choose to stop and continuously change direction
+			This allows it to get the sphere to any orientation? Bit surprising!
+			It is on S2. Can only go in 1 direction
+			When it grabs, it rotates 90 degrees, so now it is rotating around the point it grabbed
+			So it can
+
+		What about icosahedron <-> hexacross?
+
+
+	Once you've found the point to stabilize, you "explore the three sphere" by just rotating, of course
+
+
+
+	Interesting visualization would be hopf fibration morphed to S2xS1 thing you had in mind
+
+	https://ourworldindata.org/about
+	https://careerssearch.bbc.co.uk/jobs/job/RD-Engineer/39502
+
+	Another reason not to be surprised: physics clearly motivated some breakthroughs in maths, this is the head-and-head argument you make.
+	Differentials
+	quaternions
+	symplectic geometry
+	Maybe riemannian geometry because he thought space might be curved?
+
+	The fuck is the lie algebra of something?
+/*
