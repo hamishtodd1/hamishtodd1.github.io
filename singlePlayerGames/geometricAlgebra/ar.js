1c2a0f02c9f9310c2158009958f2426676e686ae
hamish <hamish.todd1@gmail.com>
2020-01-26 00:55:56 +0000

    added some stuff

---
+++ singlePlayerGames/geometricAlgebra/ar.js
@@ -0,0 +1,26 @@
+/*
+	Possibly this will enthuse people more than your own ideas for levels ever could
+
+	put a dot or QR code on things
+
+	Detect edges
+
+	Friend does dance, you try to model their dance
+
+	Speed, slow etc
+
+	Could be done on a normal phone for god's sake
+
+	Some CV stuff may help you anyway, if you use footage
+
+	Regid bodies only
+
+	With Joan. Train a neural net, extract the bivector
+
+	People make little things out of lego? Cogs?
+*/
+
+function initAr()
+{
+	
+}
