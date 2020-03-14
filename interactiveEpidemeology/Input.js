b70560392b01aa14ceb10c3b0afbc68f99cac355
hamish <hamish.todd1@gmail.com>
2020-03-14 12:45:51 +0000

    screw absolute links

---
+++ interactiveEpidemeology/Input.js
@@ -0,0 +1,79 @@
+var InputObject = { //only allowed to use this in this file, and maybe in init
+	MousePosition: new THREE.Vector3(),
+	isMouseDown: 0
+};
+
+function ReadInput(Users, ControllerModel,Models)
+{
+	isMouseDown_previously = isMouseDown;
+	isMouseDown = InputObject.isMouseDown;
+	
+	MousePosition.copy(InputObject.MousePosition);
+}
+
+document.addEventListener( 'mousemove', function(event)
+{
+	event.preventDefault();
+	
+	var vector = new THREE.Vector3(
+		  ( event.clientX / window.innerWidth ) * 2 - 1,
+	    - ( event.clientY / window.innerHeight) * 2 + 1,
+	    0.5 );
+	vector.unproject( Camera );
+	var dir = vector.sub( Camera.position ).normalize();
+	var distance = - Camera.position.z / dir.z;
+	var finalposition = Camera.position.clone();
+	finalposition.add( dir.multiplyScalar( distance ) );
+	
+	InputObject.MousePosition.copy(finalposition);
+}, false );
+
+
+document.addEventListener( 'mousedown', function(event) 
+{
+	event.preventDefault();
+	
+	InputObject.isMouseDown = 1;
+}, false );
+
+document.addEventListener( 'mouseup', function(event) 
+{
+	event.preventDefault();
+	
+	InputObject.isMouseDown = 0;
+}, false );
+
+//not document? ok, whatever
+window.addEventListener( 'resize', Resize, false );
+
+
+function Resize() 
+{	
+	var min_height = (VIEWBOX_HEIGHT + VIEWBOX_SPACING ) * 2;
+	var corresponding_width = min_height / window.innerHeight * window.innerWidth;
+	var min_width = (VIEWBOX_WIDTH + VIEWBOX_SPACING ) * 2;
+	
+	//the part of the plane z = 0 that the camera can see
+	var playing_field_width;
+	var playing_field_height;
+
+	if( min_width > corresponding_width)
+	{
+		playing_field_width = min_width;
+		playing_field_height = min_width / window.innerWidth * window.innerHeight;
+	}
+	else
+	{
+		playing_field_height = min_height; //world units
+		playing_field_width = min_height / window.innerHeight * window.innerWidth;
+	}
+
+	
+	var vertical_fov = 2 * Math.atan( (playing_field_height/2) / cameradist );
+	
+	Camera.fov = vertical_fov * 360 / TAU;
+	
+	Camera.aspect = window.innerWidth / window.innerHeight;
+	Camera.updateProjectionMatrix();
+	Renderer.setSize( window.innerWidth, window.innerHeight );
+}
---
+++ interactiveEpidemeology/VideoStuff.js
@@ -0,0 +1,121 @@
+function makeTextSign(initialText)
+{
+	var canvas = document.createElement("canvas");
+	var context = canvas.getContext("2d");
+	var material = new THREE.MeshBasicMaterial({ map: new THREE.CanvasTexture(canvas) });
+
+	let currentText = ""
+	material.setText = function (text)
+	{
+		if (currentText === text)
+			return
+
+		currentText = text
+
+		var font = "Trebuchet"
+		var backgroundMargin = 50;
+		var textSize = 100;
+		context.font = textSize + "pt " + font;
+		var textWidth = context.measureText(text).width;
+		canvas.width = textWidth + backgroundMargin;
+		canvas.height = textSize + backgroundMargin;
+
+		context.font = textSize + "pt " + font;
+		context.textAlign = "center";
+		context.textBaseline = "middle";
+
+		var backGroundColor = "#3F3D3F"
+		context.fillStyle = backGroundColor;
+		context.fillRect(
+			canvas.width / 2 - textWidth / 2 - backgroundMargin / 2,
+			canvas.height / 2 - textSize / 2 - backgroundMargin / 2,
+			textWidth + backgroundMargin,
+			textSize + backgroundMargin);
+
+		var textColor = "#D3D1D3"
+		context.fillStyle = textColor;
+		context.fillText(text, canvas.width / 2, canvas.height / 2);
+
+		this.map.needsUpdate = true;
+
+		sign.scale.x = canvas.width / canvas.height * sign.scale.y;
+
+		//the geometry isn't affected ofc
+	}
+
+	var geo = new THREE.PlaneBufferGeometry(1., 1.)
+	var sign = new THREE.Mesh(geo, material);
+
+	sign.scale.x = canvas.width / canvas.height * sign.scale.y;
+
+	material.setText(initialText);
+
+	return sign;
+}
+
+let clickThereSign = makeTextSign("<- Click there to play and pause!")
+clickThereSign.scale.multiplyScalar(.3)
+clickThereSign.position.x = 4.2
+
+function initVideo()
+{	
+	// create the video element
+	video = document.createElement( 'video' );
+	video.src = "http://hamishtodd1.github.io/Sysmic/Data/Movie_0002.mp4";
+	video.crossOrigin = "anonymous";
+	
+	 video.id = 'video';
+	 video.type = ' video/mp4; codecs="theora, vorbis" ';
+	
+	video.load(); // must call after setting/changing source
+	video.play();
+	
+//	video.currentTime = 290; //don't worry about the first column
+	
+	var videoImage = document.createElement( 'canvas' );
+	videoImage.width = 640;
+	videoImage.height = 480;
+
+	videoImageContext = videoImage.getContext( '2d' );
+	// background color if no video present
+	videoImageContext.fillStyle = '#000000';
+	videoImageContext.fillRect( 0, 0, videoImage.width, videoImage.height );
+
+	videoTexture = new THREE.Texture( videoImage );
+	videoTexture.minFilter = THREE.LinearFilter;
+	videoTexture.magFilter = THREE.LinearFilter;
+	
+	movieScreen = new THREE.Mesh( 
+			new THREE.PlaneGeometry( VIEWBOX_WIDTH,VIEWBOX_HEIGHT ),
+			new THREE.MeshBasicMaterial( { map: videoTexture, overdraw: true, side:THREE.DoubleSide } ) );
+	movieScreen.position.set(0,0,0);
+
+	movieScreen.add(boundingbox.clone());
+	
+	Scene.add(movieScreen);
+
+	Scene.add(clickThereSign)
+}
+
+function update_video()
+{
+	if( typeof video !== 'undefined' && video.readyState === video.HAVE_ENOUGH_DATA)
+	{
+		videoImageContext.drawImage( video, 0, 0 );
+		if ( videoTexture ) 
+			videoTexture.needsUpdate = true;
+	}
+	
+	if( Math.abs(MousePosition.x) < VIEWBOX_WIDTH / 2
+		&& Math.abs(MousePosition.y) < VIEWBOX_HEIGHT / 2 
+		&& isMouseDown && !isMouseDown_previously)
+	{
+		if( video.paused && !video.ended )
+			video.play();
+		else
+			video.pause();
+	}
+
+	if(video.currentTime > 2.)
+		Scene.remove(clickThereSign)
+}
