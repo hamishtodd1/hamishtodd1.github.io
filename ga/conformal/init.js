0470d5d33adf2f19646359ec57aab1a7565071e4
unknown <img.localadmin@NP-H-TODD.kl.imgtec.org>
2023-04-24 19:29:31 +0100

    arguably the most minimal three vr thing

---
+++ ga/conformal/init.js
@@ -0,0 +1,14 @@
+/*
+    TODO
+        Because animation, want a timeline with parameters you can put on
+        Make a texture you can draw on
+        A tube with three bones in it
+ */
+
+function init() {
+
+    let a = new THREE.Mesh(new THREE.SphereGeometry(.2), new THREE.MeshBasicMaterial({color:0xFF0000}))
+    scene.add(a)
+
+
+}
