7fda38923ec3798797d06bb186999fa5137f527b
hamishtodd1 <hamish.todd1@gmail.com>
2024-01-02 16:53:58 +0000

    mrh

--- ga/quantum/variables.js
+++ ga/quantum/variables.js
@@ -1,4 +1,11 @@
 v0 = new THREE.Vector3()
+v1 = new THREE.Vector3()
+v2 = new THREE.Vector3()
+v3 = new THREE.Vector3()
+v4 = new THREE.Vector3()
+v5 = new THREE.Vector3()
+v6 = new THREE.Vector3()
+v7 = new THREE.Vector3()
 
 const camera = new THREE.PerspectiveCamera(0., 0., 1., 10.)
 
